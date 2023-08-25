62c038070bbc2fd12342f10373e54ff607006321
tinganjoseph <tinganjoseph7@gmail.com>
2023-08-25 10:12:06 +0000

    installation of php on linux

---
+++ Installation/PHP.php
@@ -0,0 +1,7 @@
+/*
+!Install PHP
+!First, make sure PHP is installed on your Ubuntu machine
+
+*sudo apt update
+*sudo apt install php php-cli php-fpm php-json php-common php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath
+*/
