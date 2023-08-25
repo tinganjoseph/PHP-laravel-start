4d13f0aa19e64e77bc26efef51802c993cdca36a
tinganjoseph <tinganjoseph7@gmail.com>
2023-08-25 10:24:15 +0000

    ..installation of laravel

---
+++ Installation/laravel.php
@@ -0,0 +1,10 @@
+/*
+ todo: Install Laravel Once Composer is installed, you can install Laravel globally:
+
+ *composer global require laravel/installer
+ todo: Make sure to add Composer's global bin directory to your PATH:
+
+*echo 'export PATH="$PATH:$HOME/.composer/vendor/bin"' >> ~/.bashrc source ~/.bashrc
+
+
+*/
