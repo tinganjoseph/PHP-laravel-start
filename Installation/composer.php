edb100eb31ed2b4493d6d683e89869700beaf83f
tinganjoseph <tinganjoseph7@gmail.com>
2023-08-25 10:16:47 +0000

    installation of composer

---
+++ Installation/composer.php
@@ -0,0 +1,9 @@
+/*
+todo:Install Composer
+todo:Composer is a dependency manager for PHP. You'll need this to install Laravel and other dependencies:
+    *curl -sS https://getcomposer.org/installer | php sudo mv composer.phar /usr/local/bin/composer
+
+
+
+
+*/
